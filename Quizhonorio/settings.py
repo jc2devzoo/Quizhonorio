b9703705ab714a13859658aa7d25f70d62690dca
jc2devzoo <juliorocha0207@gmail.com>
2022-05-12 21:07:01 -0300

    hs
    Signed-off-by: jc2devzoo <juliorocha0207@gmail.com>

--- Quizhonorio/settings.py
+++ Quizhonorio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o+j3m-5d@n&w@+0=9vm^gea*poj#k9&jugu^6wb-xa__9^gvv3'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -78,7 +79,7 @@ WSGI_APPLICATION = 'Quizhonorio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': config('DB_NAME'),
     }
 }
 
